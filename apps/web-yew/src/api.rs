72eaa459d5b6b7bcfb2b0366a58c3bee11f892f9
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-06-06 03:30:42 +0100

    refactor(web-yew): needless question mark

--- apps/web-yew/src/api.rs
+++ apps/web-yew/src/api.rs
@@ -6,18 +6,18 @@ use common::types::{LengthenResponse, ShortenRequest, ShortenResponse};
 pub async fn shorten(url: String) -> Result<ShortenResponse, Error> {
     let body = ShortenRequest { url };
 
-    Ok(Request::post("/api")
+    Request::post("/api")
         .json(&body)?
         .send()
         .await?
         .json()
-        .await?)
+        .await
 }
 
 pub async fn lengthen(id: String) -> Result<LengthenResponse, Error> {
-    Ok(Request::get(format!("/api/{id}").as_str())
+    Request::get(format!("/api/{id}").as_str())
         .send()
         .await?
         .json()
-        .await?)
+        .await
 }
