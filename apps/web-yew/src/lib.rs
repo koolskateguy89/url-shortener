6f400657ad2d6380e7c9e1a14f6764fd0961896c
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-07-03 00:58:54 +0100

    tests(web-yew): support wasm tests

---
+++ apps/web-yew/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod api;
+pub mod components;
+pub mod hooks;
+pub mod routes;
--- apps/web-yew/src/main.rs
+++ apps/web-yew/src/main.rs
@@ -1,21 +1,10 @@
-use yew::prelude::*;
-use yew_router::prelude::*;
-
 mod api;
+mod app;
 mod components;
 mod hooks;
 mod routes;
 
-use routes::{switch, Route};
-
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <BrowserRouter>
-            <Switch<Route> render={switch} />
-        </BrowserRouter>
-    }
-}
+use app::App;
 
 fn main() {
     yew::Renderer::<App>::new().render();
