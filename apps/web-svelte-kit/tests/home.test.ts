cef424f578e4d008d5f6adad960d5b1d2a533f1b
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-07-04 22:18:30 +0100

    tests(web-svelte-kit): add home page playwright integ tests and noop vitest unit test

---
+++ apps/web-svelte-kit/tests/home.test.ts
@@ -0,0 +1,30 @@
+import { expect, test } from '@playwright/test';
+
+test('home page has expected input with "type" and "name" of "url"', async ({ page }) => {
+	await page.goto('/');
+
+	const inputElem = page.getByRole('textbox', { name: 'url' });
+	await expect(inputElem).toBeVisible();
+	await expect(inputElem.getAttribute('type')).resolves.toBe('url');
+});
+
+test('ID of shortened gets displayed', async ({ page, context }) => {
+	context.route('**/api/url/shorten', (route) => {
+		route.fulfill({
+			status: 200,
+			contentType: 'application/json',
+			body: JSON.stringify({ id: 'test_id' })
+		});
+	});
+
+	await page.goto('/');
+
+	await page.getByRole('textbox', { name: 'url' }).fill('https://www.google.com');
+
+	await page.click('button[type=submit]');
+	// await page.waitForResponse('**/api/url/shorten');
+
+	const link = page.locator('a[href]');
+	await expect(link).toBeVisible();
+	await expect(link.getAttribute('href')).resolves.toBe('/test_id');
+});
--- apps/web-svelte-kit/tests/test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('home page has expected input with type and name of "url"', async ({ page }) => {
-	await page.goto('/');
-
-	const inputElem = page.getByRole('textbox', { name: 'url' });
-	await expect(inputElem).toBeVisible();
-	await expect(inputElem.getAttribute('type')).resolves.toBe('url');
-});
