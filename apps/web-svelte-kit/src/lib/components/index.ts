297785ac723dd542be12fe9485e16818440ce41f
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-07-04 20:26:45 +0100

    feat(web-svelte-kit): tailwind indicator

---
+++ apps/web-svelte-kit/src/lib/components/index.ts
@@ -0,0 +1,1 @@
+export { default as TailwindIndicator } from './TailwindIndicator.svelte';
--- apps/web-svelte-kit/src/routes/+layout.svelte
+++ apps/web-svelte-kit/src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
+	import { dev } from '$app/environment';
+	import { TailwindIndicator } from '$components';
+
 	import '../app.postcss';
 </script>
 
+<svelte:head>
+	<title>URL Shortener</title>
+</svelte:head>
+
 <slot />
+
+{#if dev}
+	<TailwindIndicator />
+{/if}
